28dcd3969eb66a22bc68808ec5492f97d5851c47
YARunya37 <aroslavklucikov34199@gmail.com>
2026-04-13 11:36:18 +0500

    Метод restoreState + комментарии
    - Создан метод, который при запуске программы восстанавливает состояние виджета файловой системы. Все файлы добавленные пользователем ВРЕМЕННО КОПИРУЮТСЯ в выделенную папку. Метод читает эти файлы и отображает их виджете
    - Сделаны комментарии ко всем методом внутри FileTreeWidget

--- src/ui/filetreewidget.cpp
+++ src/ui/filetreewidget.cpp
@@ -1,7 +1,10 @@
 #include "filetreewidget.h"
 #include <QFileDialog>
+#include <QFile>
+#include <QDir>
 #include <QDebug>
-#include <QFileSystemModel>
+#include <QCoreApplication>
+
 FileTreeWidget::FileTreeWidget(QWidget* parent) :
     QTreeWidget(parent)
 {
@@ -10,43 +13,84 @@ FileTreeWidget::FileTreeWidget(QWidget* parent) :
 
 void FileTreeWidget::AddFiles()
 {
+    // Вызываем диалоговое окно, в котором указываются файлы
     QStringList files = QFileDialog::getOpenFileNames(
         this,
         "Выбрать файлы",
         "",
         "Текстовые документы (*.docx *.md *.pdf)"
     );
+
+    // Все указанные файлы конвертируем и добавляем в папку с файлами проекта
     foreach (auto filePath, files) {
         QString name = GetName(filePath).split(".")[0];
+
         // Если файла с таким именем ещё нет, то добавляем его в дерево
         if(!localFiles.contains(name)){
-            localFiles.insert(name, filePath);
-            QTreeWidgetItem* new_item = new QTreeWidgetItem(this);
-            new_item->setText(0, name);
+            // Путь к директории с файлами(внутри проекта) + имя данного файла с расширением
+            QString dest = QCoreApplication::applicationDirPath() + "/resources/userfiles/"+ GetName(filePath);
+
+            // ЗДЕСЬ ДОЛЖНА БЫТЬ КОНВЕРТАЦИЯ ФАЙЛОВ И ЗАПИСЬ ИХ В НУЖНУЮ ПАПКУ
+            if(QFile::copy(filePath, dest)){
+                // Добавляем в map локальный файл
+                localFiles.insert(name, dest);
+
+                // Создаем новую строчку в виджете с именем файла
+                QTreeWidgetItem* new_item = new QTreeWidgetItem(this);
+                new_item->setText(0, name);
+            }
+            // СДЕЛАТЬ УВЕДОМЛЕНИЕ ЧТО ФАЙЛЫ УЖЕ ДОБАЛЕНЫ
         }
     }
 }
 
 void FileTreeWidget::setUpTree()
 {
+    // Визуальная часть
     setHeaderLabel("Материалы");
     setMinimumWidth(50);
     setMaximumWidth(200);
     setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Expanding));
     setColumnCount(1);
+
+    // Восстанавливаем состояние на основе уже добавленных файлов
+    restoreState();
 }
 
 QString FileTreeWidget::GetName(QString file)
 {
     QString name = "";
+    // Циклом справа налево записываем символы в строку
     for (int i = file.length()-1; i >= 0; i--) {
         if(file[i] != "/" && file[i] != "\\"){
             name = file[i] + name;
         }
         else{
+            // Если мы дошли до / или \, то имя записано полностью
             break;
         }
     }
+
     return name;
+}
+
+void FileTreeWidget::restoreState()
+{
+    // Путь к директориии с файлами, добавленными пользователем
+    QString localfilesPath = QCoreApplication::applicationDirPath() + "/resources/userfiles/";
+    // Получение всех файлов внури директории localfilesPath
+    QStringList existing_files = QDir(localfilesPath).entryList(QDir::Files);
+
+    // Каждый файл отображаем в виджете и заново добавляем в map локальных файлов
+    foreach (auto file, existing_files) {
+        QString name = GetName(file).split(".")[0];
+
+        // Добавление в map
+        localFiles.insert(name, localfilesPath + file);
+
+        // Создаем новую строчку в виджете с именем файла
+        QTreeWidgetItem* new_item = new QTreeWidgetItem(this);
+        new_item->setText(0, name);
+    }
 }
 
--- src/ui/filetreewidget.h
+++ src/ui/filetreewidget.h
@@ -5,19 +5,25 @@
 #include <QStringList>
 #include <QMap>
 #include <QString>
+// Виджет для отображения и управления файлами внутри проекта
 class FileTreeWidget : public QTreeWidget
 {
     Q_OBJECT
 public:
     FileTreeWidget(QWidget* parent = nullptr);
 public slots:
+    // Вызывает диалоговое окно для добавления выбранных файлов в виджет файловой системы
     void AddFiles();
 private:
     // Массив файлов в проекте <имя файла(ключ), путь к нему(значение)>
+    // Внутри проекта все файлы имеют путь /resources/userfiles/
     QMap<QString, QString> localFiles;
+    // Создаёт виджет(форма, стиль и необходимый функционал)
     void setUpTree();
-    // Возвращает имя файла С РАСШИРЕНИЕМ(.docx .md ...)
-    QString GetName(QString file);
+    // Возвращает имя файла С РАСШИРЕНИЕМ(.docx .md ...) по его пути
+    QString GetName(QString filePath);
+    // Отображает уже сохранённые пользователем файлы
+    void restoreState();
 };
 
 #endif // FILETREEWIDGET_H
