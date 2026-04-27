95ffd5c6fc2b2fa73105d13f418b3b1ef29764a7
YARunya37 <aroslavklucikov34199@gmail.com>
2026-04-27 15:41:50 +0500

    Уменьшен интервал сохранения после изменений

--- src/ui/studycard.cpp
+++ src/ui/studycard.cpp
@@ -22,7 +22,7 @@ StudyCardWidget::StudyCardWidget(QWidget *parent, const QString& local_path_to_c
     // Используем таймер, который по истечении вызовет сохранение текущего состояния виджетов
     QTimer *saveTimer = new QTimer(this);
     saveTimer->setSingleShot(true);
-    saveTimer->setInterval(2000); // 2 секунды после последнего изменения
+    saveTimer->setInterval(1000); // 2 секунды после последнего изменения
 
     connect(header, &QTextEdit::textChanged, [saveTimer]() {
         saveTimer->start(); // перезапускаем таймер
