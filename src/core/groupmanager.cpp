ee168e157d07fcbbd4c2703b1046cf8115b60dcf
YARunya37 <aroslavklucikov34199@gmail.com>
2026-04-27 13:13:15 +0500

    Список групп в groupmanager

--- src/core/groupmanager.cpp
+++ src/core/groupmanager.cpp
@@ -1,5 +1,11 @@
 #include "groupmanager.h"
-
+#include <QCoreApplication>
 GroupManager::GroupManager(QObject *parent)
-    : QObject{parent}
-{}
+    : QObject{parent},
+    DirItemsManager(QCoreApplication::applicationDirPath() + "/resources/usergroups")
+{
+    // Инициализируем список
+    foreach (auto item, RestoreItems()) {
+        groups.insert(item, new Group(item));
+    }
+}
--- src/core/groupmanager.h
+++ src/core/groupmanager.h
@@ -2,14 +2,21 @@
 #define GROUPMANAGER_H
 
 #include <QObject>
+#include "diritemsmanager.h"
+#include <QMap>
+#include "group.h"
 
-class GroupManager : public QObject
+class GroupManager : public QObject, public DirItemsManager
 {
     Q_OBJECT
 public:
     explicit GroupManager(QObject *parent = nullptr);
 
 signals:
+
+private:
+    // Список всех групп по именам
+    QMap<QString, Group*> groups;
 };
 
 #endif // GROUPMANAGER_H
