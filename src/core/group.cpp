b0ad4acc4a68eadd84a707e650a17de443211b9c
YARunya37 <aroslavklucikov34199@gmail.com>
2026-04-27 13:24:50 +0500

    Создание группы билетов, получение и очистка

--- src/core/group.cpp
+++ src/core/group.cpp
@@ -1,5 +1,24 @@
 #include "group.h"
+#include <QCoreApplication>
 
 Group::Group(const QString& name)
-    : name{name}
-{}
+    : name{name},
+    DirItemsManager(QCoreApplication::applicationDirPath() + "/resources/usergroups/" + name)
+{
+    foreach(auto item, RestoreItems()){
+        auto card = new StudyCardWidget();
+        cards.insert(item, card);
+    }
+}
+
+StudyCardWidget* Group::GetCard(const QString &name) const
+{
+    return cards.value(name);
+}
+
+void Group::Clear()
+{
+    foreach (auto item, cards.keys()) {
+        DeleteItem(item);
+    }
+}
--- src/core/group.h
+++ src/core/group.h
@@ -3,17 +3,25 @@
 
 #include <QString>
 #include "studycard.h"
+#include <QMap>
+#include "diritemsmanager.h"
 
 // Класс, отвечающий за конкретную группу пользователя
-class Group
+class Group : public DirItemsManager
 {
 public:
     Group(const QString& name);
     // Метод для получение имени группы
     QString Name() const;
+    // Метод для получения указателя на билет по имени
+    StudyCardWidget* GetCard(const QString& name) const;
+    // Метод для удаления всех билетов в группе
+    void Clear();
 private:
     // Поле с именем группы
     QString name;
+    // Список билетов
+    QMap<QString, StudyCardWidget*> cards;
 };
 
 #endif // GROUP_H
